73a32165d063d72a95ba32f753791864f6bab4d2
Ice Baibolov <baiba865@newschool.edu>
2016-09-23 12:53:53 +0200

    IceB

---
+++ IceB/js/raster.js
@@ -0,0 +1,19 @@
+//DRAWING
+var canvas  = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height= window.innerHeight;
+var ctx = canvas.getContext("2d");
+var image = document.getElementById("andy");
+
+function setup(){
+
+}
+
+function draw(){
+
+      
+}
+
+
+setup();
+draw();
